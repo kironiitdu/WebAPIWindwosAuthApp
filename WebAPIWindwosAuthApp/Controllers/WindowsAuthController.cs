18489f03a7b7d3d4dbaeadf99b607d4e3ca54c6d
Md Farid Uddin <v-mdud@microsoft.com>
2023-05-08 13:38:22 +0800

    Add project files.

---
+++ WebAPIWindwosAuthApp/Controllers/WindowsAuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIWindwosAuthApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WindowsAuthController : ControllerBase
+    {
+        [HttpGet("GetUserWindowsUserName")]
+        public IActionResult GetUserWindowsUserName()
+        {
+            string gettingWindowsUser = HttpContext.User.Identity?.Name!;
+            var responseMessage = string.Format("Current Windows User Name: {0}", gettingWindowsUser);
+            return Ok(responseMessage);
+        }
+    }
+}
